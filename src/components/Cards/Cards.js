c2789f73279a5ce86bb0f8921e86a696ba292f48
Mark Boyd <mark.boyd1414@gmail.com>
2021-03-18 20:38:32 -0400

    moved loading text below pagination so user can still change pages if pokemon data is not able to be fetched

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -4,26 +4,26 @@ import Card from '../Card/Card';
 export default function Cards({ loading, pokemonData, prev, next }) {
   return (
     <div>
+      <div className="btn">
+        <button onClick={prev}>
+          <i
+            className="fa fa-chevron-left"
+            style={{ marginRight: '5px', fontSize: '14px' }}
+          ></i>
+          Prev
+        </button>
+        <button onClick={next}>
+          Next{' '}
+          <i
+            className="fa fa-chevron-right"
+            style={{ marginLeft: '5px', fontSize: '14px' }}
+          ></i>
+        </button>
+      </div>
       {loading ? (
         <h1 style={{ textAlign: 'center' }}>Loading...</h1>
       ) : (
         <>
-          <div className="btn">
-            <button onClick={prev}>
-              <i
-                class="fa fa-chevron-left"
-                style={{ marginRight: '5px', fontSize: '14px' }}
-              ></i>
-              Prev
-            </button>
-            <button onClick={next}>
-              Next{' '}
-              <i
-                class="fa fa-chevron-right"
-                style={{ marginLeft: '5px', fontSize: '14px' }}
-              ></i>
-            </button>
-          </div>
           <div className="grid-container">
             {pokemonData.map((pokemon, i) => {
               return <Card key={i} pokemon={pokemon} />;
